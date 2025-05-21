1d9eeeef014444e93d64697590519e4661ab4525
Jay <44783760+jschenke488@users.noreply.github.com>
2025-05-21 04:31:36 -0500

    change the redirect URL to an embed

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const pipedApi = Deno.args[0].toString()
 const handler = async (request) => {
-    
     if (!(request.method == 'GET' && '/'+request.url.split('/').slice(3).join('/') == '/')) {
         return new Response('404 Not Found', {
             status: 404,
@@ -16,7 +15,7 @@ const handler = async (request) => {
     const streams = await tabData.json()
     const videoGameStudyLounge = streams.content.find(stream => stream.duration == -1 && stream.title.toLowerCase().includes('video game study lounge'))
     if (videoGameStudyLounge) {
-        const videoGameStudyLoungeUrl = `https://www.youtube.com${videoGameStudyLounge.url}`
+        const videoGameStudyLoungeUrl = `https://www.youtube-nocookie.com/embed/${videoGameStudyLounge.url.split('=')[1]}?autoplay=1`
         return Response.redirect(videoGameStudyLoungeUrl, 302)
     } else return new Response('500 Internal Server Error', {
         status: 500,
